9add21d43b6218c91a9e5496613880d7bda16f52
Ben Northway <northben@pixelchef.net>
2016-01-18 18:54:42 -0500

    adding django project

---
+++ splunkdjango/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for splunkdjango project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "splunkdjango.settings")
+
+application = get_wsgi_application()
